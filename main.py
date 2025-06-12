fea93064301ca8c77af9cb8acb8c9db32ca87c85
Fionax8 <fiona.kalinowska@gmail.com>
2025-06-12 20:54:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from rewe_scraper import get_offers
+from rewe_scraper import scrape_rewe_offers
 
 app = FastAPI()
 
 @app.get("https://rewe-angebote-api.onrender.com/angebote")
 def read_offers():
-    return get_offers()
+    return scrape_rewe_offers()
